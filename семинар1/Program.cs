f59ecf810d0b9cf8e41897df330c485d4bf670eb
Alyona <a_rasta@mail.ru>
2022-08-21 14:41:49 +0400

    Задача 2

---
+++ семинар1/Program.cs
@@ -0,0 +1,13 @@
+﻿int num;
+
+Console.Write("ВВедите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while(current<=num){
+    Console.Write(current + "");
+    current++;
+}
+
+
