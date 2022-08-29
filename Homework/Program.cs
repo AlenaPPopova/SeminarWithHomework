101a0fa7536ecdf3e9b4866f4d77039438839590
Alyona <a_rasta@mail.ru>
2022-08-29 14:13:20 +0400

    Дз к 2 семинару

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -98,3 +98,117 @@
 // }
 // //Вычисление и ответ
 
+
+
+
+
+                           //*****Дз по семинару2*****
+
+// //  Задача 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// //показывает вторую цифру этого числа.
+
+// int Number(){
+//     int num = new Random().Next(100,1000);
+//     Console.WriteLine("Number is - " + num);
+//     int dec = num / 10;
+//     int ed = dec % 10;
+//     int result = ed;
+//     return result;
+// }
+
+// int number = Number();
+// Console.WriteLine("Number is -" + number);
+
+
+
+// // // //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// // что третьей цифры нет.
+// Не решена. В процессе.
+   
+// int Number(){  
+//     int num = number;
+//     Console.WriteLine("Number is - ");
+//     if (num>=100){
+//         int dec = num / 10;
+//         int ed = num % 10;
+//         int result = dec*10 + ed;
+//         return result;
+//         }
+//         else
+//         {
+//             Console.WriteLine("Третьей цифры нет");
+//         }
+    
+// }
+
+// Console.WriteLine("Enter number -");
+// int Number(num);
+
+
+
+
+
+// // int Number(){
+// //     int num = new Random().Next(10,1000);
+// //     Console.WriteLine("Number is - " + num);
+// //     if(num>=100){
+// //         int dec = num / 10;
+// //         int ed = dec % 100;
+// //         int result = ed;
+// //         }
+
+// //     else {
+// //         Console.WriteLine("Two-digit number.");
+// //     }
+// //     return num;
+// // }
+
+// // int number = Number();
+// // Console.WriteLine("Number is - " + number);
+
+
+
+
+
+
+// // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// //  и проверяет, является ли этот день выходным.
+
+
+// void Week(int num){
+//     if (num == 6 || num == 7){
+//     Console.WriteLine("Is day off");
+//     }
+//     else if (num < 1 || num > 7){
+//     Console.WriteLine("No day of the week!!!");
+//     }
+//     else{
+//     Console.WriteLine("Weekdays");
+//     }
+// }
+// Console.Write("Enter number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Week(num);
+
+
+
+
+
+
+
+
+
+                    // ***** Дз по семинару3*****
+
+// // Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125                    
